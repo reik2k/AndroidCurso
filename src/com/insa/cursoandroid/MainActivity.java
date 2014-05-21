29722c2844ed2e69819d0f1fe63e35300eba7ea9
abel2k <reik2k@hotmail.com>
2014-05-21 08:15:06 +0200

    Terminado el Ejemplo Falta Depurar!!

--- src/com/insa/cursoandroid/MainActivity.java
+++ src/com/insa/cursoandroid/MainActivity.java
@@ -2,18 +2,42 @@ package com.insa.cursoandroid;
 
 import com.insa.cursoandroid.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
-
+	static EditText txt_protocolo;
+	static Button 	btn_Navegar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main); 
 
+		txt_protocolo = (EditText)findViewById(R.id.txtTexto);
+		btn_Navegar = (Button)findViewById(R.id.btnNav);
+		
+		btn_Navegar.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this,NavegadorActivity.class);
+				
+				Bundle b = new Bundle();
+				
+				b.putString("param1",txt_protocolo.getText().toString());
+				
+				i.putExtras(b);
+				startActivity(i);
+				
+			}
+		});
 		
 	}
 

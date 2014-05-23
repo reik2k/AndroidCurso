dbc1a4c3dec1e705db22626fd08f037f2933282c
abel2k <reik2k@hotmail.com>
2014-05-23 08:19:28 +0200

    Terminado Activities 1 y 2 falta probar el paso de parámetros entre ambas

--- src/com/insa/cursoandroid/MainActivity.java
+++ src/com/insa/cursoandroid/MainActivity.java
@@ -2,19 +2,40 @@ package com.insa.cursoandroid;
 
 import com.insa.cursoandroid.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-
+	TextView 	txt;
+	Button 		btn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main); 
 
+		txt = (TextView)findViewById(R.id.txtPantalla1);
+		btn	= (Button)findViewById(R.id.btnIr);
 		
+		btn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Bundle bundle = new Bundle();
+				Intent i = 
+						new Intent(MainActivity.this,Pantalla2Activity.class);
+				
+				bundle.putString("param1", txt.getText().toString());
+				i.putExtras(bundle);
+				startActivity(i);
+			}
+		});
 	}
 
 	@Override
---
+++ src/com/insa/cursoandroid/Pantalla2Activity.java
@@ -0,0 +1,94 @@
+package com.insa.cursoandroid;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Build;
+
+public class Pantalla2Activity extends Activity {
+	
+	Button 		btnVolver;
+	TextView 	txtVolver;
+	Bundle 		bundle;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pantalla2);
+		
+		btnVolver 	= (Button)findViewById(R.id.bntVolver);
+		txtVolver 	= (TextView)findViewById(R.id.txtDos);
+		
+		bundle = getIntent().getExtras();
+		
+		txtVolver.setText(bundle.getString("param1"));
+		
+		btnVolver.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) 
+			{
+				Intent i = getIntent();
+				bundle.putString(
+						"param1","Pantalla2 "+txtVolver.getText().toString());
+				
+				setResult(RESULT_OK,i);
+				finish();
+				
+			}
+		});
+		
+		
+		/*if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}*/
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.pantalla2, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_pantalla2,
+					container, false);
+			return rootView;
+		}
+	}*/
+
+}

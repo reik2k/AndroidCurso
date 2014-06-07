19cd286fc45b303c9a12eb852fe22f95d3f2d733
abel2k <reik2k@hotmail.com>
2014-06-07 18:32:48 +0200

    Ejemplo Terminado, la clase contiene el código de ejemplo funcionando

--- src/com/insa/cursoandroid/MainActivity.java
+++ src/com/insa/cursoandroid/MainActivity.java
@@ -3,16 +3,68 @@ package com.insa.cursoandroid;
 import com.insa.cursoandroid.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends Activity {
-
+	TextView		txtFontSize;
+	ToggleButton	tglSetFontSize;
+	Button			btnFontSize;
+	EditText		edtFontSizeIn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main); 
+		setContentView(R.layout.activity_main);
+		
+		txtFontSize		=	(TextView)findViewById(R.id.textView1);
+		tglSetFontSize	=	(ToggleButton)findViewById(R.id.tglButton);
+		btnFontSize		=	(Button)findViewById(R.id.button1);
+		edtFontSizeIn	=	(EditText)findViewById(R.id.editText1);
+		
+		//To create the listeners for the activity
+		//TOGGLEBUTTON
+		tglSetFontSize.setOnClickListener(new View.OnClickListener() 
+		{
+			public void onClick(View v) 
+			{
+				if(tglSetFontSize.isChecked())
+				{
+					txtFontSize.setTextSize(30);
+					Log.d("tglSetFontSize","Tamaño del EditText es 30");
+				}else
+					{
+						txtFontSize.setTextSize(10);
+						Log.d("tglSetFontSize","Tamaño del EditText es 30");
+					}
+				
+			}
+		});
+		
+		//BUTTON
+		btnFontSize.setOnClickListener(new View.OnClickListener() 
+		{
+			public void onClick(View v) 
+			{
+				if(tglSetFontSize.isChecked())
+				{
+					edtFontSizeIn.setTextSize(30);
+					Log.d("btnFontSize","Tamaño del EditText es 30");
+				}else
+					{
+						edtFontSizeIn.setTextSize(10);
+						Log.d("btnFontSize","Tamaño del EditText es 10");
+					}
+				
+			}
+		});
+		
 
 		
 	}

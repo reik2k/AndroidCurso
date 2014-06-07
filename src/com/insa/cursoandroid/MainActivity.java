06385e94a2b2b467be64cd38301bab0b4009df27
abel2k <reik2k@hotmail.com>
2014-06-07 13:40:30 +0200

    Se termina el ejemplo con los TextView y el ToggleButton

--- src/com/insa/cursoandroid/MainActivity.java
+++ src/com/insa/cursoandroid/MainActivity.java
@@ -5,15 +5,39 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends Activity {
 
+	ToggleButton tglButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main); 
-
+		setContentView(R.layout.activity_main);
+		
+		tglButton = (ToggleButton)findViewById(R.id.toggleButton1);
+		
+		tglButton.setOnClickListener(new View.OnClickListener() 
+		{
+			
+			public void onClick(View v) 
+			{
+				TextView	txt = (TextView)findViewById(R.id.textView1);
+				
+				if(tglButton.isChecked())
+				{
+					txt.setText("Botón Activado!");
+				}else
+				{
+					txt.setText("Desactivado!!");
+				}
+				
+			}
+		});
 		
 	}
 

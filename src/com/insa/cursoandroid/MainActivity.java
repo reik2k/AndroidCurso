a03ccf5287bf5ac2c56d0e40ad73a45188ddcc8c
abel2k <reik2k@hotmail.com>
2014-06-07 19:44:26 +0200

    Se añade el RadioGroup con Log.i

--- src/com/insa/cursoandroid/MainActivity.java
+++ src/com/insa/cursoandroid/MainActivity.java
@@ -2,14 +2,16 @@ package com.insa.cursoandroid;
 
 import com.insa.cursoandroid.R;
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -18,6 +20,7 @@ public class MainActivity extends Activity {
 	ToggleButton 	tglButton;
 	CheckBox		chkBox;
 	ImageView		img;
+	RadioGroup		radOption;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class MainActivity extends Activity {
 		
 		tglButton 	= (ToggleButton)findViewById(R.id.toggleButton1);
 		chkBox		= (CheckBox)findViewById(R.id.checkBox1);
+		radOption	= (RadioGroup)findViewById(R.id.radGroup);
 		img			= (ImageView)findViewById(R.id.imgBadChiken);
 		
 		//ToggelButton Listener 
@@ -48,24 +52,60 @@ public class MainActivity extends Activity {
 		});
 		
 		//CheckBox Lsitener
-		
 		chkBox.setOnCheckedChangeListener(
-				new CheckBox.OnCheckedChangeListener()
-				{
+			new CheckBox.OnCheckedChangeListener()
+			{
 
-					public void onCheckedChanged(	CompoundButton buttonView,
-													boolean isChecked) 
+				public void onCheckedChanged(	CompoundButton buttonView,
+												boolean isChecked) 
+				{
+					RadioButton rad;
+					if(isChecked)
 					{
-						if(isChecked)
+						Log.i("CheckBox","Imagen del Pollo");
+						rad = (RadioButton)findViewById(R.id.radPollo);
+						img.setImageResource(R.drawable.bad_chicken);
+						rad.setChecked(true);
+						
+					}else
 						{
-							img.setImageResource(R.drawable.bad_chicken);
-						}else
-							{
-								img.setImageResource(R.drawable.ic_launcher);
-							}
+							Log.i("CheckBox","Imagen del Android");
+							rad = (RadioButton)findViewById(R.id.radAndroid);
+							img.setImageResource(R.drawable.ic_launcher);
+							rad.setChecked(true);
+						}
+				
+				}
+			});
+		
+		//RadioGroupListener
+		radOption.setOnCheckedChangeListener(
+			new RadioGroup.OnCheckedChangeListener() 
+			{
+			
+				public void onCheckedChanged(	RadioGroup group, 
+												int checkedId) 
+				{
+					RadioButton radPollo =	
+							(RadioButton)findViewById(R.id.radPollo);
+					RadioButton radAnd =	
+							(RadioButton)findViewById(R.id.radAndroid);
 					
+					if(radPollo.isChecked())
+					{
+						Log.i("raOption","[Opcion1] Pollo");
+						img.setImageResource(R.drawable.bad_chicken);
+						chkBox.setChecked(true);
+						
 					}
-				});
+					if(radAnd.isChecked())
+					{
+						Log.i("raOption","[Opcion2] Android");
+						img.setImageResource(R.drawable.ic_launcher);
+						chkBox.setChecked(false);
+					}
+				}
+			});
 		
 	}
 

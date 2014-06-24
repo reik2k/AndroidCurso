ba3eef1e291e7d06c8dc3488add27a3ff27e23e4
abel2k <reik2k@hotmail.com>
2014-06-24 22:00:18 +0200

    Se termina el ejercicio 1 Tema4

--- src/com/insa/cursoandroid/MainActivity.java
+++ src/com/insa/cursoandroid/MainActivity.java
@@ -5,15 +5,73 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
 public class MainActivity extends Activity {
-
+	
+	private CheckBox 	chk;
+	private RadioButton	rdSingle;
+	private RadioButton	rdMarried;
+	private RadioButton	rdOther;
+	private EditText	edtTextAge;
+	private EditText	edtTextState;
+	private Button		btn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main); 
-
+		
+		//Seleccionamos el Botón al iniciar la activity
+		btn = (Button)findViewById(R.id.btnLegalAge);
+		
+		//Declaramos un listener para manejar el boton
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) 
+			{
+				//Declaramos los widgets a utilizar
+				chk 			= (CheckBox)findViewById(R.id.chkLegalAge);
+				rdSingle		= (RadioButton)findViewById(R.id.radSingle);
+				rdMarried		= (RadioButton)findViewById(R.id.radMarried);
+				rdOther			= (RadioButton)findViewById(R.id.radOther);
+				edtTextAge		= (EditText)findViewById(R.id.edtLegaAge);
+				edtTextState 	= (EditText)findViewById(R.id.edtCivilState);
+				
+				//Borramos el contenido anterior de los TEXTview.
+				edtTextAge.setText("");
+				edtTextState.setText("");
+				
+				if(chk.isChecked())
+				{
+					edtTextAge.setText("Eres Mayor de Edad.");
+				}else
+					{
+						edtTextAge.setText("NO Eres Mayor de Edad.");
+					}
+				
+				if(rdMarried.isChecked())
+				{
+					edtTextState.setText("Estás Casado.");
+				}
+				
+				if(rdSingle.isChecked())
+				{
+					edtTextState.setText("Estás Soltero");
+				}
+				if(rdOther.isChecked())
+				{
+					edtTextState.setText("Tu estado civil es indefinido");
+				}
+			}
+		});
+		
 		
 	}
 

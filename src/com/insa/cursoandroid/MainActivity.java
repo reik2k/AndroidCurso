54dda28939816a2cb37395ef98b5e9e70c770525
abel2k <reik2k@hotmail.com>
2014-06-28 12:51:23 +0200

    Ejemplo1 y 2 temrinado para tratar HEX, OCT, BIN y DEC numbers

--- src/com/insa/cursoandroid/MainActivity.java
+++ src/com/insa/cursoandroid/MainActivity.java
@@ -10,26 +10,53 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
-	TextView	txt;
-	static	final String NEW_LINE = "\n"; 
+	TextView				txt;
+	static	final String 	NEW_LINE = "\n"; 
+	static	String			result;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main); 
-
-		setExample();
+		
+		txt = (TextView)findViewById(R.id.txtResult);
+		
+		result = "EJEMPLO1" + NEW_LINE;
+		result += "-----------------------------" + NEW_LINE;
+		
+		result += setExample();
+		
+		result += NEW_LINE + "EJEMPLO2" + NEW_LINE;
+		result += "-----------------------------" + NEW_LINE;
+		
+		result += setExample1();
+		
+		txt.setText(result);
 	}
 	
-	private void setExample()
+	private String setExample1()
+	{
+		String numBinary		=	"101111";
+		String numOctal			=	"57";
+		String numHexadecimal	=	"2F";
+		String numDecimal		=	"47";
+		
+		String	resultExample1	=	"";
+		
+		resultExample1	+= 	Integer.parseInt(numBinary,2)		+	NEW_LINE;
+		resultExample1	+= 	Integer.parseInt(numOctal,8)		+	NEW_LINE;
+		resultExample1	+= 	Integer.parseInt(numHexadecimal,16)	+	NEW_LINE;
+		resultExample1	+= 	Integer.parseInt(numDecimal,10)		+	NEW_LINE;
+		
+		return resultExample1;
+	}
+	private String setExample()
 	{
 		String 	strScreen;
 		Integer	num1	= 	new Integer("22");
 		Integer num2	= 	new Integer("100");
 		int		n1		= 	num1.intValue();
 		int		n2		= 	num2.intValue();
-		
-		txt = (TextView)findViewById(R.id.txtResult);
 		
 		strScreen = "La suma de num1 ("+n1+") + num2 ("+n2+") "+
 					"es igual a " + (n1+n2)+NEW_LINE;
@@ -46,9 +73,9 @@ public class MainActivity extends Activity {
 			n1--;
 		}
 		
-		strScreen += ", entonces n1 = "+n1;
-		txt.setText(strScreen);
+		strScreen += ", entonces n1 = " + n1 + NEW_LINE;
 		
+		return strScreen;
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {

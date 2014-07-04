31210f080f96d7a9b264e4346ba84f86d686c366
abel2k <reik2k@hotmail.com>
2014-07-04 19:29:59 +0200

    Finalizado

--- src/com/insa/cursoandroid/MainActivity.java
+++ src/com/insa/cursoandroid/MainActivity.java
@@ -3,20 +3,73 @@ package com.insa.cursoandroid;
 import com.insa.cursoandroid.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.telephony.gsm.SmsManager;
+import android.telephony.gsm.SmsMessage;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-
+	
+	private EditText	edtPhone;
+	private	EditText	edtMessage;
+	private Button		btnSend;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main); 
-
+		
+		//Init Widgets
+		btnSend 	= (Button)findViewById(R.id.btnSend);
+		edtPhone 	= (EditText)findViewById(R.id.edtPhone);
+		edtMessage 	= (EditText)findViewById(R.id.edtMessage);
+		
+		btnSend.setOnClickListener(new View.OnClickListener() 
+		{
+			public void onClick(View v) 
+			{
+				if(	validateSMS(	edtPhone.getText().toString(),
+									edtMessage.getText().toString()))
+				{
+					sendSMS(	edtPhone.getText().toString(),
+								edtMessage.getText().toString());
+					
+				}else
+				{
+					Toast.makeText(	getBaseContext(), 
+									"You have to fill both fields", 
+									Toast.LENGTH_SHORT).show();
+				}
+				
+			}
+		});
 		
 	}
-
+	
+	protected void sendSMS(String phone, String smsText)
+	{
+		SmsManager sms = SmsManager.getDefault();
+		
+		sms.sendTextMessage(phone, null, smsText, null, null);
+		
+	}
+	
+	protected boolean validateSMS(String phone, String sms)
+	{
+		Boolean response = false;
+		
+		if(phone.isEmpty() || sms.isEmpty())
+		{
+			return response;
+		}
+		
+		return true;
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
